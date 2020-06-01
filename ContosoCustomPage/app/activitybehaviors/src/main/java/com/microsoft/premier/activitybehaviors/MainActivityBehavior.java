362a05d10d1e03aa78882a23ac7a15dd1edf856a
Christiano Donke <chrido@microsoft.com>
2020-06-01 15:32:40 -0300

    Update Java Project

--- ContosoCustomPage/app/activitybehaviors/src/main/java/com/microsoft/premier/activitybehaviors/MainActivityBehavior.java
+++ ContosoCustomPage/app/activitybehaviors/src/main/java/com/microsoft/premier/activitybehaviors/MainActivityBehavior.java
@@ -12,14 +12,19 @@ public class MainActivityBehavior {
     private Button button_increment;
     private TextView counter_label;
 
-    public MainActivityBehavior(Activity mainActivity) {
-        _activity = mainActivity;
+    public MainActivityBehavior(Activity activity) {
+        _activity = activity;
     }
 
-    public void SetupInterface(int R_id_counter_label, int R_id_button_increment_counter){
-        counter_label = _activity.findViewById(R_id_counter_label);
+    /*public void SetupInterface(TextView label, Button button){
+        counter_label = label;
+        button.setOnClickListener(mButtonListener);
+    }*/
 
-        button_increment = _activity.findViewById(R_id_button_increment_counter);
+    public void SetupInterface(Integer R_id_label, Integer R_id_button){
+        counter_label = _activity.findViewById(R_id_label);
+
+        button_increment = _activity.findViewById(R_id_button);
         button_increment.setOnClickListener(mButtonListener);
     }
 
